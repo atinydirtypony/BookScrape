c1b2615a97bf75485fb0428e7cafe00c28b1d269
Russell Fillmore <russellfillmore@Davids-MacBook-Pro-3.local>
2019-06-23 15:19:57 -0400

    csv working html write is not

--- spiders/cliff_spider.py
+++ spiders/cliff_spider.py
@@ -18,15 +18,31 @@ class CliffSpider(scrapy.Spider):
             parent_folder_name = re.sub('[ &\'\.,:\(\)!]','_',book.css('div.note-name h4::text').get())
             follow_link = book.css('div.info a::attr(href)').get()
             if(not os.path.isdir('Results/'+parent_folder_name)):
-                print('Making folder: Results/'+parent_folder_name)
                 os.mkdir('Results/'+parent_folder_name)
 
-            parser = self.book_note_parse_factory(parent_folder_name)
+            parser = self.book_page_parse_factory(parent_folder_name)
             yield scrapy.Request('https://www.cliffsnotes.com'+follow_link, parser)
 
-    def book_note_parse_factory(self,parent_folder):
+    def book_page_parse_factory(self,parent_folder):
         def F(response):
-            print(parent_folder)
-            with open('Results/'+parent_folder+'/first_page.html', 'wb') as f:
-                f.write(response.body)
+            csv_index = ''
+            for nav_link in response.css('section.secondary-navigation ul li a'):
+                link_adress = nav_link.css('::attr(href)').get()
+                page_name = nav_link.css('span::text').get()
+                csv_index = csv_index + page_name+','+link_adress.split("/")[-1]+'\n'
+                parser = self.chapter_page_parse_factory(parent_folder);
+                yield scrapy.Request('https://www.cliffsnotes.com'+link_adress, parser)
+            with open('Results/'+parent_folder+'/index.csv', 'wb') as csv:
+                csv.write(csv_index.encode())
         return F
+
+    def chapter_page_parse_factory(self,parent_folder):
+        def F(response):
+            page = response.url.split("/")[-1]
+            filename = 'Results/'+parent_folder+'/'+page+'.html'
+            print(filename)
+            try:
+                with open(filename, 'wb') as file:
+                    file.write(response.body)
+            except Exception as e:
+                print(e)

df2db576e59ecb5ff95658805747100bcac08ebb
Russell Fillmore <russellfillmore@Davids-MacBook-Pro-3.local>
2019-06-23 14:34:55 -0400

    Follow parse working now

--- spiders/cliff_spider.py
+++ spiders/cliff_spider.py
@@ -17,17 +17,15 @@ class CliffSpider(scrapy.Spider):
         for book in response.css('li.note'):
             parent_folder_name = re.sub('[ &\'\.,:\(\)!]','_',book.css('div.note-name h4::text').get())
             follow_link = book.css('div.info a::attr(href)').get()
-            print('Follow Link: '+follow_link)
             if(not os.path.isdir('Results/'+parent_folder_name)):
                 print('Making folder: Results/'+parent_folder_name)
                 os.mkdir('Results/'+parent_folder_name)
 
             parser = self.book_note_parse_factory(parent_folder_name)
-            print(parser)
-            response.follow(follow_link, parser)
+            yield scrapy.Request('https://www.cliffsnotes.com'+follow_link, parser)
 
     def book_note_parse_factory(self,parent_folder):
-        def F(current_self, response):
+        def F(response):
             print(parent_folder)
             with open('Results/'+parent_folder+'/first_page.html', 'wb') as f:
                 f.write(response.body)
